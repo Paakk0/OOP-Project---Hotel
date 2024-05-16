120bbdb553d8bdd4d79a807b55117b3940d1ec14
Християн Керкенезов <71761678+Paakk0@users.noreply.github.com>
2024-05-17 00:41:03 +0300

    Add files via upload

--- UnavailableCommand.java
+++ UnavailableCommand.java
@@ -5,12 +5,24 @@ import Model.Hotel;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
+/**
+ * Represents a command to mark a room as unavailable for a specified duration.
+ */
 public class UnavailableCommand extends Command {
 
+    /**
+     * Executes the command to mark a room as unavailable for a specified duration.
+     *
+     * @param args The arguments for the command. It should contain the room number,
+     *             start date, end date, and a note.
+     */
     @Override
     public void Command(List<String> args) {
         if (args.size() == 4) {
-                Hotel.unavailable(Integer.parseInt(args.get(0)), convertDateToString(args.get(1)), convertDateToString(args.get(2)), args.get(3));
-        } else System.out.println("This command requires 4 argument!");
+            Hotel.unavailable(Integer.parseInt(args.get(0)), convertDateToString(args.get(1)),
+                    convertDateToString(args.get(2)), args.get(3));
+        } else {
+            System.out.println("This command requires 4 arguments!");
+        }
     }
 }
